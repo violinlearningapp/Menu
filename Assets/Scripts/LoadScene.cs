ef762249a60de9469b29f222b7edd6b3112bf511
Ville Vainio <ville.vainio92@gmail.com>
2018-01-16 09:19:16 +0200

    lol

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    // exitBtn.onClick.AddListener(() => { Application.Quit(); });
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+}
+    
